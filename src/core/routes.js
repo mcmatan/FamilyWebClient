46e31ced6c457c19dd0f5787a23263a40566fd75
MCMatan <cohen.abravanel@gmail.com>
2016-12-06 00:27:29 +0100

    Login stays in and same for logout, saved with firebase

--- src/core/routes.js
+++ src/core/routes.js
@@ -4,25 +4,19 @@ import App from '../ui/components/App';
 import LoginComponent from '../ui/components/LogInComponent';
 import Dashboard from '../ui/components/Dashboard';
 import Welcome from '../ui/components/Welcome';
+import {firebaseAuth} from '../core/firebase';
 
 
-
-function MatchWhenAuthed ({component: Component, authed, ...rest}) {
-    return (
-        <Match
-            {...rest}
-            render={(props) => authed === true
-                ? <Component {...props} />
-                : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
-        />
-    )
-}
-
+const confirmAuth = (nextState, replace) => {
+    if (!firebaseAuth.currentUser) {
+        replace({ pathname: '/', state: { nextPathname: nextState.location.pathname } });
+    }
+};
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={Welcome} />
         <Route path="login" component={LoginComponent}/>
-        <Route path="dashboard" component={Dashboard} />
+        <Route path="dashboard" component={Dashboard} onEnter={confirmAuth} />
     </Route>
 );
